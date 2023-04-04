713a4fe8df74947a6acc8669d3291a23ff9de394
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-04 15:23:08 +0200

    add studentList

---
+++ Snack2/README.md
@@ -0,0 +1,12 @@
+SNACK 2
+======
+
+Abbiamo un elenco degli studenti di una facoltà
+Ogni studente ha un nome, un cognome, un numero di matricola e un elenco di voti.
+Dobbiamo creare un nuovo elenco dove ogni studente ha un nome-cognome, matricola e media voti.
+*** BONUS *** <br>
+Aggiungere la foto o l’avatar dello studente e stampare tutti gli studenti con delle card HTML in ordine alfabetico per cognome;
+
+## STEPS
+- Creo un elenco di studenti;
+
---
+++ Snack2/js/script.js
@@ -0,0 +1,86 @@
+// Ogni studente ha un nome, un cognome, un numero di matricola e un elenco di voti.
+const studentList = [
+
+{
+  firstname: 'Mirko',
+  lastname:  'Martorana',
+  universityNumber: 01,
+  grades: [
+  italiano   = 30,
+  filosofia  = 25,
+  psicologia = 20,
+  inglese    = 18,
+  francese   = 30
+  ]
+},
+
+{
+  firstname:  'Katia',
+  lastname:   'Follesa',
+  universityNumber: 02,
+  grades: [
+  italiano   = 30,
+  filosofia  = 25,
+  psicologia = 20,
+  inglese    = 18,
+  francese   = 30
+  ]
+},
+
+{
+  firstname:   'Fabio',
+  lastname:    'Balsamo',
+  universityNumber: 03,
+  grades: [
+  italiano   = 30,
+  filosofia  = 25,
+  psicologia = 20,
+  inglese    = 18,
+  francese   = 30
+  ]
+},
+
+{
+  firstname:  'Ciro',
+  lastname:   'Priello',
+  universityNumber: 04,
+  grades: [
+  italiano   = 30,
+  filosofia  = 25,
+  psicologia = 20,
+  inglese    = 18,
+  francese   = 30
+  ]
+},
+
+{
+  firstname:   'Filippo',
+  lastname:      'Fanti',
+  universityNumber: 05,
+  grades: [
+  italiano   = 30,
+  filosofia  = 25,
+  psicologia = 20,
+  inglese    = 18,
+  francese   = 30
+  ]
+},
+
+{
+  firstname: 'Luca',
+  lastname:  'Argentero',
+  universityNumber: 06,
+  grades: [
+  italiano   = 30,
+  filosofia  = 25,
+  psicologia = 20,
+  inglese    = 18,
+  francese   = 30
+  ]
+}
+
+]
+
+
+
+

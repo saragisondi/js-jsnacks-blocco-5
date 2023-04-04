c005e515e90a16728644e59c4dfdd7539abec5dd
Sara Gisondi <sara.gisondi@outlook.it>
2023-04-04 14:29:56 +0200

    first commit

---
+++ Snack1/README.md
@@ -0,0 +1,8 @@
+SNACK 1
+======
+
+Abbiamo un elenco utenti (nome, cognome, età).
+Trovare i minorenni e gli over 65
+
+## STEPS
+
---
+++ Snack1/js/script.js
@@ -0,0 +1,39 @@
+const listUser = [
+  {
+  nome: 'Edoardo',
+  cognome: 'Bianchi',
+  eta: 17,
+  },
+
+  {
+  nome: 'Marco',
+  cognome: 'Giallini',
+  eta: 66,
+  },
+
+  {
+  nome: 'Chiara',
+  cognome: 'Rossi',
+  eta: 15,
+  },
+
+  {
+  nome: 'Fabio',
+  cognome: 'Volo',
+  eta: 70,
+  },
+
+  {
+  nome: 'Tommaso',
+  cognome: 'Bernini',
+  eta: 10,
+  },
+
+  {
+  nome: 'Sara',
+  cognome: 'Gisondi',
+  eta: 30,
+  },
+
+
+]
